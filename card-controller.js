7c2af7f2a6711c520df57799c93747372945545b
Darryl Kilzer <darrylkilzer@gmail.com>
2018-02-28 13:23:16 -0700

    pulling in a game and hand

--- card-controller.js
+++ card-controller.js
@@ -1,3 +1,34 @@
 function CardController(){
   var cardService = new CardService()
+
+  function drawCards(){
+    var cardsElem = document.getElementById('cards')
+    var cardTemplate = ''
+    var cards = cardService.getCards()
+
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i];
+      var random = Math.floor(Math.random() * 3) + 1
+      cardTemplate +=`
+      <div class="col-6 col-md-3">
+          <div class="grad${random} pad">
+            <div class="card-header bg-info text-white margin">
+              <p>Card ${i+1}</p>
+            </div>
+            <div class="card-block rgba-green-light text-white">
+              <img style="width: 100%" src="${card.img}" alt="">
+              <p class="">Health: ${card.health}</p>
+            </div>
+            <div class="card-footer rgba-grey-light text-white clearfix">
+              <i class="ra ra-sword pull-left"> ${card.attack}</i>
+              <i class="ra ra-knight-helmet pull-right"> ${card.defense}</i>
+            </div>
+          </div>
+        </div>
+      `
+    }
+    cardsElem.innerHTML = cardTemplate
+  }
+
+  cardService.loadCards(drawCards)
 }
--- card-service.js
+++ card-service.js
@@ -1,3 +1,51 @@
-function CardService(){
-  
+function CardService() {
+
+  var api = axios.create({
+    baseURL: 'https://inspire-server.herokuapp.com/cards',
+    timeout: 1000,
+  })
+
+  var game
+  var cards
+
+  // var cards = [
+  //   {
+  //     name: 'highland sword',
+  //     img: 'sword.png',
+  //     description: 'A basic sword used for battle.',
+  //     attack: 2,
+  //     defense: 1,
+  //     style: 'highland-sword'
+  //   },
+  //   {
+  //     name: 'elven battle sword',
+  //     img: 'sword.png',
+  //     description: 'An elegant sword used for battle.',
+  //     attack: 2,
+  //     defense: 2,
+  //     style: 'elven-sword'
+  //   },
+  //   {
+  //     name: 'short battle sword',
+  //     img: 'sword.png',
+  //     description: 'A short sword used for battle.',
+  //     attack: 1,
+  //     defense: 3,
+  //     style: 'short-sword'
+  //   }
+  // ]
+
+  this.getCards = function(){
+    return cards
+  }
+
+  this.loadCards = function(cb){
+    api().then(res =>{
+      game = res.data[0]
+      cards = game.players[0].hand
+      console.log(game)
+      cb()
+    })
+  }
+
 }
